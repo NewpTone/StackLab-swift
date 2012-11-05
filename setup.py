ec58f78910b4ff8d504710ec626af383f6dff80d
Alex Yang <alex890714@gmail.com>
2012-11-05 09:37:33 +0800

    add quota in setup.py

--- setup.py
+++ setup.py
@@ -95,6 +95,7 @@ setup(
             'name_check=swift.common.middleware.name_check:filter_factory',
             'proxy_logging=swift.common.middleware.proxy_logging:'
                 'filter_factory',
+            'quota=swift.common.middleware.quota:filter_factory',
             ],
         },
     )
